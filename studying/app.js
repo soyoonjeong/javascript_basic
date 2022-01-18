1d5d1a01d2477bbfaa7ae05168759148d7ac8010
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2022-01-18 22:14:46 +0900

    add studying

---
+++ studying/app.js
@@ -0,0 +1,106 @@
+const player = {
+    name:"soyoon",
+    sayHello :function(otherPersonsName){
+        console.log("hello! "+otherPersonsName);
+    }
+};
+//console.log(player.name);
+//player.sayHello(player.name);
+
+const calculator = {
+    plus : function(a,b){
+        return a+b;
+    },
+    minus : function(a,b){
+        return a-b;
+    },
+    times : function(a,b){
+        return a*b;
+    },
+    divide : function(a,b){
+        return a/b;
+    },
+    power : function(a,b){
+        return a**b;
+    }
+};
+
+const title = document.getElementById("title"); //객체 반환
+//title.innerText = "i changed it!";
+//console.log(title.id);
+//console.log(title.innerText);
+
+const hellos = document.getElementsByClassName("hello"); //Array 반환
+//console.log(hellos);
+
+let h1s = document.getElementsByTagName("h1"); //Array 반환
+//console.log(h1s);
+
+h1s = document.querySelectorAll(".hello h1"); //Array 반환
+//console.log(h1s);
+const titles = document.querySelectorAll("#title"); //Array 반환
+//console.log(titles);
+
+const h1 = document.querySelector(".hello h1"); 
+// 조건을 만족하는 첫번째 element만 가져옴
+//console.log(h1);
+//console.dir(h1);
+//h1.style.
+function handleClick(){
+    /*
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor==='blue'){
+        newColor = 'tomato';
+    }else{
+        newColor = 'blue';
+    }
+    h1.style.color = newColor; */
+
+    /*
+    const clickedClass = 'clicked background-color';
+    if(h1.className===clickedClass){
+        h1.className = "background-color";
+    }else{
+        h1.className = clickedClass
+    }*/
+    
+    /*
+    const clickedClass = "clicked";
+    if(h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
+    }else{
+        h1.classList.add(clickedClass);
+    }*/
+
+    h1.classList.toggle("clicked"); //있으면 삭제, 없으면 추가
+}
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "pink";
+}
+function handleWindowCopy(){
+    alert("copier");
+}
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+function handleWindowOnline(){
+    alert("GOOD");
+}
+h1.addEventListener("click",handleClick);
+//h1.click = handleClick;
+h1.addEventListener("mouseenter",handleMouseEnter);
+//h1.mouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave",handleMouseLeave);
+//h1.mouseleave = handleMouseLeave;
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
