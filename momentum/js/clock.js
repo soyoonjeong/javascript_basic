473e59481b1ae87002e4fe7bb47654a79f5917a2
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2022-01-19 00:26:18 +0900

    add clock

---
+++ momentum/js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("#clock");
+
+function getClock(){
+    const date = new Date();
+    const month = String(date.getMonth()+1).padStart(2,"0");
+    const day = String(date.getDate()).padStart(2,"0");
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    today.innerText = `${date.getFullYear()}/${month}/${day}`
+}
+
+getClock();
+setInterval(getClock, 1000);
